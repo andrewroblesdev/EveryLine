47c0c537f7bd4eb99bc102cf5fa577692d048966
Andrew Robles <andrewroblesdev@gmail.com>
2021-01-14 19:26:30 -0800

    Make demo section mobile responsive

--- components/demo/demo.js
+++ components/demo/demo.js
@@ -4,13 +4,14 @@ import styles from './demo.module.css';
 import Editor from '../editor/editor';
 
 const exampleCode = 
-`function fibonacci(n) {
+`// Fibonacci
+function fib(n) {
   if (n === 2) {
     return 1;
   } else if (n === 1) {
     return 0;
   } else {
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    return fib(n - 1) + fib(n - 2);
   }
 }`   
 
