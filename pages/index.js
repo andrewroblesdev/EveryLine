1b73ab9958f772fac337769be87516b9c91be4a3
Andrew Robles <andrewroblesdev@gmail.com>
2021-01-15 14:42:32 -0800

    Add webpage title

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import About from '../components/about/about';
 export default function Home() {
   return (
     <div>
+      <title>EveryLine</title>
       <Navbar />
       <Header />
       <About />
