ab6d6c22c5c174583e4f7b98d4f979d2b12afe49
Andrew Robles <andrewroblesdev@gmail.com>
2021-01-18 02:04:42 -0800

    Determine syntax highlighting based on file extension

--- components/player/player.js
+++ components/player/player.js
@@ -1,9 +1,24 @@
+import { useState } from 'react'
+
 import {UnControlled as CodeMirror} from 'react-codemirror2'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/dracula.css'
 
 if (typeof navigator !== 'undefined') {
     require('codemirror/mode/javascript/javascript')
+    require('codemirror/mode/python/python')
+    require('codemirror/mode/shell/shell')
+    require('codemirror/mode/css/css')
+    require('codemirror/mode/dockerfile/dockerfile')
+    require('codemirror/mode/go/go')
+    require('codemirror/mode/php/php')
+    require('codemirror/mode/r/r')
+    require('codemirror/mode/ruby/ruby')
+    require('codemirror/mode/scheme/scheme')
+    require('codemirror/mode/sql/sql')
+    require('codemirror/mode/swift/swift')
+    require('codemirror/mode/vb/vb')
+    require('codemirror/mode/xml/xml')
 }
 
 import Tab from './tab/tab'
@@ -13,18 +28,28 @@ import 'bootstrap/dist/css/bootstrap.css'
 import styles from './player.module.css'
 
 export default function Player() {
+    const [state, setState] = useState({
+        'fileType': ''
+    })
+
+    const updateFileTypeState = (newFileType) => {
+        setState({
+            'fileType': newFileType
+        })
+    }
+
     return(
         <div className={`${styles.player}`}>
             <div className={`container`}>
                 <div className={`${styles.editor}`}>
                     <div className={`${styles.navbar}`}>
-                        <Tab title='Untitled'/>
+                        <Tab title='Untitled' updateFileTypeState={updateFileTypeState} />
                         <Button />
                     </div>
                     <CodeMirror
                         value={''}
                         options={{
-                            mode: 'javascript',
+                            mode: state.fileType,
                             theme: 'dracula',
                             lineNumbers: true,
                             readOnly: false
@@ -36,3 +61,4 @@ export default function Player() {
         </div>
     )
 }
+
