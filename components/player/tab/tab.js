807a4dc126280d7210873f6decc4dc3a4027ae43
Andrew Robles <andrewroblesdev@gmail.com>
2021-01-18 01:09:40 -0800

    Enable users to change tab name by clicking to type

--- components/player/tab/tab.js
+++ components/player/tab/tab.js
@@ -3,7 +3,7 @@ import styles from './tab.module.css'
 export default function Tab(props) {
     return(
         <div className={`${styles.tab}`}>
-            <p>{props.title}</p>
+            <input className={`form-control ml-4`} placeholder="Untitled"></input>
         </div>
     )
 }
