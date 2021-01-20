8b261374efc1efc2384c8458fa886fabc7f258f4
Andrew Robles <andrewroblesdev@gmail.com>
2021-01-19 23:36:38 -0800

    Update sign in message to a try it out link and add a link back home

--- components/lead/lead.js
+++ components/lead/lead.js
@@ -1,38 +1,12 @@
-import {useState} from 'react';
-
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-
 import styles from './lead.module.css';
 
-export default function Lead() {
-    const [show, setShow] = useState(false);
+import Link from 'next/link'
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
+export default function Lead() {  
     return (
-      <>
-        <button type="button" className={`btn ${styles.btnCustom}`} onClick={handleShow}>Sign up</button>
-
-        <Modal show={show} onHide={handleClose}>
-
-          <Modal.Header closeButton>
-            <Modal.Title>A message from the developer</Modal.Title>
-          </Modal.Header>
-
-          <Modal.Body>
-            <p>Hey there!<br/><br/>The core functionality of the platform is still being worked on but will hopefully be able to support user sign ups very soon. Thanks for stopping by!<br/><br/>-Andrew</p>
-          </Modal.Body>
-        
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleClose}>
-              Okay
-            </Button>
-          </Modal.Footer>
-
-        </Modal>
-      </>
+      <Link href="/demo">
+        <a className={`btn ${styles.btnCustom}`}>Try It Out</a>
+      </Link>
     );
   
 }
