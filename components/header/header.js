cbfcd2c2bb095a77ca9218a02080de4d75d00680
Adrian Valenzuela <avalenzuela85@gmail.com>
2021-01-30 01:37:14 -0800

    added nicer curve and adjust padding and margin in header for mobile and desktop view

--- components/header/header.js
+++ components/header/header.js
@@ -17,7 +17,7 @@ export default function Header() {
   return(
     <div className="bg-everyBlue text-white pt-9">
       <div className="container mx-auto flex flex-col sm:flex-row sm:px-9">
-        <div id="lead-text" className="px-3 w-full order-last sm:order-first">
+        <div id="lead-text" className="px-3 w-full order-last mt-8 sm:mt-0 sm:order-first">
           <h2>Create coding tutorials quickly and effortlessly.</h2>
           <p className="pb-6">Everything you need in one streamlined platform.</p>
           <Lead />
@@ -26,8 +26,12 @@ export default function Header() {
           <Editor code={exampleCode}/>
         </div>
       </div>
-      <div id="white-curve" className="whitecurves">
+      <div className="hidden sm:block -mt-12">
+        <svg className="fill-current text-white" viewBox="0 0 620 20">
+          <path d="M309.498577,0 C448.456725,0 567.858086,7.91580119 620,19.2377506 L620,20 L0,20 L6.39488462e-14,19.0221788 C52.6583865,7.81544934 171.424053,0 309.498577,0 Z"></path>
+        </svg>
       </div>
     </div>
   )
 }
+
