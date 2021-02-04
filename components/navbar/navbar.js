459af953c46c5cc08167e9624a685432c6d41824
Andrew Robles <andrewroblesdev@gmail.com>
2021-02-04 05:10:12 -0800

    Center title on mobile demo view

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -6,11 +6,9 @@ import Link from 'next/link'
 export default function Navbar(props) {
     return(
         <div className={`${styles.navbar}`}>
-            <div className={`container`}>
-                <div className={`row justify-content-between`}>
-                    <div className={`col mt-4`}>
-                        { !props.isPlaying && <Link href="/"><a className={`${styles.title}`}>EveryLine</a></Link> }
-                    </div>
+            <div className={`container pb-5`}>
+                <div className={`${ styles.title } ${ styles.link }`}>
+                    { !props.isPlaying && <Link href="/"><a className={`${styles.title}`}>EveryLine</a></Link> }
                 </div>
             </div>
         </div>
