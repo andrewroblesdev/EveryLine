02cadb28ab8ae6ffaef25f696ff3adead77811ef
Andrew Robles <andrewroblesdev@gmail.com>
2021-02-09 06:11:55 -0800

    Enable google authentication

--- components/landing/landing.js
+++ components/landing/landing.js
@@ -6,6 +6,10 @@ import About from '../about/about'
 
 import Link from 'next/link'
 
+// Firebase
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
 const exampleCode = 
 `// Fibonacci
 function fib(n) {
@@ -17,6 +21,22 @@ function fib(n) {
     return fib(n-1) + fib(n-2);
   }
 }`  
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMT69nxDgfv1Xng973utYemUSeTI3UoBo",
+  authDomain: "everyline-e8c1f.firebaseapp.com",
+  projectId: "everyline-e8c1f",
+  storageBucket: "everyline-e8c1f.appspot.com",
+  messagingSenderId: "24187514806",
+  appId: "1:24187514806:web:ffc208a9c38d021f401b17",
+  measurementId: "G-5Z1F45E3DG"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+var provider = new firebase.auth.GoogleAuthProvider();
 
 export default function Landing() {
     return(
@@ -32,9 +52,9 @@ export default function Landing() {
                     <div className={`${ styles.description }`}>
                       Make coding videos 
                     </div>
-                    <Link href="/demo">
-                      <a className={`btn btn-lg btn-block ${ styles.button}`}>Get started</a>
-                    </Link>
+                    
+                      <button className={`btn btn-lg btn-block ${ styles.button}`} onClick={signIn}>Get started</button>
+                    
                   </div>
                   <div className={`${styles.editor} col`}>
                       <Editor code={exampleCode} />
@@ -45,4 +65,8 @@ export default function Landing() {
         <About className={`${ styles.about }`}/>
       </div>
     )
+}
+
+function signIn() {
+  firebase.auth().signInWithRedirect(provider)
 }
