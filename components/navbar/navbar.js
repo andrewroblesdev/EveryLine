79e500816d74419586a50f4a5347a2383b7d12bf
Andrew Robles <andrewroblesdev@gmail.com>
2021-02-11 18:55:57 -0800

    Style logout button

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -7,8 +7,15 @@ export default function Navbar(props) {
     return(
         <div className={`${styles.navbar}`}>
             <div className={`container pb-5`}>
-                <div className={`${ styles.title } ${ styles.link }`}>
-                    { !props.isPlaying && <Link href="/"><a className={`${styles.title}`}>EveryLine</a></Link> }
+                <div className={`row`}>
+                    <div classname={`col`}>
+                        <div className={`${ styles.title } ${ styles.link }`}>
+                            { !props.isPlaying && <Link href="/"><a className={`${styles.title} pl-3`}>EveryLine</a></Link> }
+                        </div>
+                    </div>
+                    <div className={`col`}>
+                        <div className={`${ styles.link } float-right mt-4`}>Log out</div>
+                    </div>
                 </div>
             </div>
         </div>
