cf2061e9e1aced6e9b4629e9f55ed117a82ec2f5
Andrew Robles <andrewroblesdev@gmail.com>
2021-02-11 19:11:51 -0800

    Sign out user on button click

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -2,8 +2,20 @@ import 'bootstrap/dist/css/bootstrap.css';
 import styles from './navbar.module.css';
 
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+// Firebase
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 export default function Navbar(props) {
+
+    const router = useRouter()
+
+    const signOutUser = () => {
+        signOut(router)
+    }
+
     return(
         <div className={`${styles.navbar}`}>
             <div className={`container pb-5`}>
@@ -14,12 +26,21 @@ export default function Navbar(props) {
                         </div>
                     </div>
                     <div className={`col`}>
-                        <div className={`${ styles.link } float-right mt-4`}>Log out</div>
+                        <button onClick={signOutUser} className={`btn btn-link ${ styles.link } float-right mt-4`}>Log out</button>
                     </div>
                 </div>
             </div>
         </div>
     )
+}
+
+function signOut(router) {
+    firebase.auth().signOut().then(() => {
+        // Sign out successful
+        router.push('/')
+    }).catch((error) => {
+        // An error happened
+    })
 }
 
 
