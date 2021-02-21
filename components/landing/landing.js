89bb78c382ebc869841c68fc436eecab50bb81cb
Andrew Robles <>
2021-02-20 21:18:24 -0800

    Update header

--- components/landing/landing.js
+++ components/landing/landing.js
@@ -6,20 +6,6 @@ import About from '../about/about'
 
 import Link from 'next/link'
 
-const exampleCode = 
-`# Get 150 day historical data 
-data = data['historical'][-150]
-
-# Create dataframe from price data
-prices = pd.DataFrame.from_dict(data)
-prices = prices.set_index('data')
-
-# Calculate 20 day moving average
-close = prices['close']
-rolling = close.rolling(window=20)
-prices['MA20'] = close.mean()
-`  
-
 export default function Landing() {
     return(
       <div>
@@ -32,14 +18,16 @@ export default function Landing() {
                 <div className={`row`}>
                   <div className={`col`}>
                     <div className={`${ styles.description }`}>
-                      Learn to build a trading bot
+                     Go viral on TechTok 
+                    </div>
+                    <div className={`${ styles.text }`}>
+                      Join a mastermind group with fellow TikTok tech content creators
                     </div>
                     <Link href="/demo">
                       <a className={`btn btn-lg btn-block ${ styles.button}`}>Get started</a>
                     </Link>
                   </div>
                   <div className={`${styles.editor} col`}>
-                      <Editor code={exampleCode} />
                   </div>
                 </div>
               </div>
