70dceb89dce183c61df11f5ac22a72948b8854df
Andrew Robles <andrewroblesdev@gmail.com>
2021-02-23 19:40:38 -0800

    Make the payment page button text less confusing

--- components/landing/landing.js
+++ components/landing/landing.js
@@ -18,11 +18,11 @@ export default function Landing() {
                      TechTok Masterclass
                     </div>
                     <div className={`${ styles.text }`}>
-                      How to create viral Tech TikToks
+                      How to create viral tech TikToks
                     </div>
                     <div className="text-center">
                       <Link href="https://launchpass.com/everyline/tech-tok-masterclass">
-                        <button type="button" className={`btn btn-lg ${ styles.button}`}><i className={`fab fa-discord mr-2`}></i>Join Discord</button>
+                        <button type="button" className={`btn btn-lg ${ styles.button}`}>Learn more</button>
                       </Link>
                     </div>
                   </div>
